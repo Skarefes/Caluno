5f37579160e009825095cd8a682408c745951128
Roger Yamassaki <skarefes@gmail.com>
2026-08-21 19:47:29 -0300

    criando a estrutura inicial do backend

---
+++ backend/src/main/java/com/yama/caluno_back/CalunoBackApplication.java
@@ -0,0 +1,13 @@
+package com.yama.caluno_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalunoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalunoBackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yama/caluno_back/CalunoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yama.caluno_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalunoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
